c5c08255536d0646120f8976a05902d454db1018
karntoptopwong-cmd <karntoptopwong@gmail.com>
2026-02-08 18:14:14 +0700

    Add files via upload

---
+++ my-web-project-main/my-web-project-main/profile.js
@@ -0,0 +1,109 @@
+const form = document.getElementById("profileForm");
+const fullname = document.getElementById("fullname");
+const classInput = document.getElementById("class");
+const number = document.getElementById("number");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const saveBtn = document.getElementById("saveBtn");
+const editBtn = document.getElementById("editBtn");
+const inputs = document.querySelectorAll("input");
+const mouseLight = document.getElementById("mouse-light");
+
+const currentUser = localStorage.getItem("currentUser");
+if (!currentUser) {
+  alert("กรุณาเข้าสู่ระบบใหม่");
+  window.location.href = "login.html";
+}
+
+const savedProfile = localStorage.getItem(`profile_${currentUser}`);
+
+if (savedProfile) {
+  const data = JSON.parse(savedProfile);
+
+  fullname.value = data.fullname || "";
+  classInput.value = data.class || "";
+  number.value = data.number || "";
+  email.value = data.email || "";
+  phone.value = data.phone || "";
+}
+
+function checkForm() {
+  if (!isEditing) return;
+
+  const isValid =
+    fullname.value.trim() !== "" &&
+    classInput.value.trim() !== "" &&
+    number.value.trim() !== "" &&
+    email.value.trim() !== "";
+
+  saveBtn.disabled = !isValid;
+  if (isValid) {
+  saveBtn.classList.add("active");
+} else {
+  saveBtn.classList.remove("active");
+}
+
+}
+
+inputs.forEach(input => {
+  input.addEventListener("input", checkForm);
+});
+
+// เริ่มต้น: ล็อกทุกช่อง
+inputs.forEach(input => input.disabled = true);
+saveBtn.disabled = true;
+
+let isEditing = false;
+
+editBtn.addEventListener('click', () => {
+  isEditing = !isEditing;
+
+  inputs.forEach(input => {
+    input.disabled = !isEditing; // ⭐ แก้ตรงนี้
+  });
+
+  saveBtn.disabled = true;
+
+  if (isEditing) checkForm();
+
+  editBtn.textContent = isEditing ? '✖ Cancel' : '✎ Edit';
+});
+
+// Save
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  /* ===== ใส่ localStorage ตรงนี้ ===== */
+  const profileData = {
+    fullname: fullname.value,
+    class: classInput.value,
+    number: number.value,
+    email: email.value,
+    phone: phone.value
+  };
+  localStorage.setItem(
+  `profile_${currentUser}`,
+  JSON.stringify(profileData)
+);
+  inputs.forEach(input => input.disabled = true);
+  saveBtn.disabled = true;
+  saveBtn.classList.remove("active");
+
+  isEditing = false;
+  editBtn.textContent = "✏ Edit";
+
+
+  alert("บันทึกข้อมูลแล้ว");
+});
+document.addEventListener("mousemove", (e) => {
+  mouseLight.style.background = `
+    radial-gradient(
+      circle at ${e.clientX}px ${e.clientY}px,
+      rgba(255, 255, 255, 0.2),
+      rgba(0, 0, 0, 0.6) 40%
+    )
+  `;
+});
+
+
+
