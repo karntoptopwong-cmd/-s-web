4d33064cf81f6c2f2120a0fb5807a3600f6b6b17
karntoptopwong-cmd <karntoptopwong@gmail.com>
2026-02-14 12:24:20 +0700

    Update help.js

--- help.js
+++ help.js
@@ -1,1 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const username = localStorage.getItem("currentUser");
+  if (!username) {
+    location.href = "index.html";
+    return;
+  }
+
+  const menuBtn = document.getElementById("menuBtn");
+  const sidebar = document.getElementById("sidebar");
+  const logoutBtn = document.getElementById("logoutBtn");
+  const homeBtn = document.getElementById("homeBtn");
+  const profileArea = document.getElementById("profileArea");
+  const mouseLight = document.getElementById("mouse-light");
+
+  // sidebar
+  menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+  });
+
+  profileArea.addEventListener("click", () => {
+    location.href = "profile.html";
+  });
+
+  homeBtn.addEventListener("click", () => {
+    location.href = "loggedin.html";
+  });
+
+  logoutBtn.addEventListener("click", () => {
+    localStorage.removeItem("currentUser");
+    location.href = "index.html";
+  });
+
+  // mouse light
+  document.addEventListener("mousemove", (e) => {
+    mouseLight.style.background = `
+      radial-gradient(
+        circle at ${e.clientX}px ${e.clientY}px,
+        rgba(255,255,255,0.18),
+        rgba(0,0,0,0.65) 40%
+      )
+    `;
+  });
+});
 
