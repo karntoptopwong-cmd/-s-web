4091ed5ae93007ac293ca74c9f9d341d62948b16
karntoptopwong-cmd <karntoptopwong@gmail.com>
2026-02-16 00:18:12 +0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://cbhfydsjuawdcgrzxzzu.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_nM-AoZn8-WSz2FQQX0zbHQ_pvcKOKAJ";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNiaGZ5ZHNqdWF3ZGNncnp4enp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExMzMxMzcsImV4cCI6MjA4NjcwOTEzN30.15OoxHWvq_6dUYeKpBEPM5HsN8tNmYH6LtcXH3gNxRM";
 
 window.supabaseClient = supabase.createClient(
   SUPABASE_URL,
