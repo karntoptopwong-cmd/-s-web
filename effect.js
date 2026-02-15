6b8cd151a260e24fd7093e6911ebdd7a1a7e21f4
karntoptopwong-cmd <karntoptopwong@gmail.com>
2026-02-15 11:28:37 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -2,6 +2,11 @@ console.log("effect.js loaded");
 
 const spotlight = document.getElementById("spotlight");
 
+if (!spotlight) {
+  console.error("spotlight not found");
+  return;
+}
+
 window.addEventListener("mousemove", (e) => {
   spotlight.style.setProperty("--x", `${e.clientX}px`);
   spotlight.style.setProperty("--y", `${e.clientY}px`);
