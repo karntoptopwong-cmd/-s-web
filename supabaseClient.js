bb4a1f8055c9d21bf07e3d1e7aaa28911da23e07
karntoptopwong-cmd <karntoptopwong@gmail.com>
2026-02-16 00:03:17 +0700

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://cbhfydsjuawdcgrzxzzu.supabase.co";
+const SUPABASE_ANON_KEY = "https://cbhfydsjuawdcgrzxzzu.supabase.co";
+
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
